e0ac49facb232ca4c5370069cb2de4b204686dbb
Adi Dahiya <adidas006@gmail.com>
2011-11-24 15:51:32 -0500

    Worked on calendar view, added favicon, and some fixes

--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -1,6 +1,8 @@
 from db.schools import create_school
 from env import env, BaseHandler
 from forms.forms import CreateSchoolForm
+from tornado.web import Application
+from tornado.web import URLSpec
 
 class CreateSchool(BaseHandler):
     template = env.get_template("admin/create_school.template")
@@ -16,5 +18,6 @@ class CreateSchool(BaseHandler):
             tags = [t.strip() for t in tags_data.split(" ") if t.strip()]
             school_name = form.school_name.data
             create_school(school_name, tags)
+            self.redirect(reverse_url('LoginHandler'))
         else:
             self.write(self.template.render(form=form))
