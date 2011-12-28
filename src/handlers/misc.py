f6a6a25f9169fd8375f518626d12b06ed0687164
Adi Dahiya <adidas006@gmail.com>
2011-12-28 18:54:30 -0500

    Added 'About' page content

--- src/handlers/misc.py
+++ src/handlers/misc.py
@@ -22,23 +22,19 @@ class LandingPage(BaseHandler):
 class About(BaseHandler):
     template = env.get_template('pages/about.template')
     def get(self):
-        navbar = self.render_navbar(None, False, True)
-        self.write(self.template.render(navbar=navbar, loginForm=LoginForm()))
+        self.write(self.template.render(loginForm=LoginForm()))
     
 class Privacy(BaseHandler):
     template = env.get_template('pages/privacy.template')
     def get(self):
-        navbar = self.render_navbar(None, False, True)
-        self.write(self.template.render(navbar=navbar, loginForm=LoginForm()))
+        self.write(self.template.render(loginForm=LoginForm()))
 
 class Terms(BaseHandler):
     template = env.get_template('pages/terms.template')
     def get(self):
-        navbar = self.render_navbar(None, False, True)
-        self.write(self.template.render(navbar=navbar, loginForm=LoginForm()))
+        self.write(self.template.render(loginForm=LoginForm()))
         
 class Contact(BaseHandler):
     template = env.get_template('pages/contact.template')
     def get(self):
-        navbar = self.render_navbar(None, False, True)
-        self.write(self.template.render(navbar=navbar, loginForm=LoginForm()))
+        self.write(self.template.render(loginForm=LoginForm()))
